6a7f4b67f55e9516e7470fce7d03a8acbd7f5120
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-03-30 16:22:21 +0800

    feat: #4 Chat UI
    #4

---
+++ app/src/main/kotlin/com/google/ai/soga/GenerativeAiViewModelFactory.kt
@@ -0,0 +1,36 @@
+package com.google.ai.soga
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.generationConfig
+import com.google.ai.soga.feature.chat.ChatViewModel
+
+val GenerativeViewModelFactory = object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(
+        viewModelClass: Class<T>,
+        extras: CreationExtras
+    ): T {
+        val config = generationConfig {
+            temperature = 0.7f
+        }
+
+        return with(viewModelClass) {
+            when {
+                isAssignableFrom(ChatViewModel::class.java) -> {
+                    // Initialize a GenerativeModel with the `gemini-pro` AI model for chat
+                    val generativeModel = GenerativeModel(
+                        modelName = "gemini-1.0-pro",
+                        apiKey = BuildConfig.apiKey,
+                        generationConfig = config
+                    )
+                    ChatViewModel(generativeModel)
+                }
+
+                else ->
+                    throw IllegalArgumentException("Unknown ViewModel class: ${viewModelClass.name}")
+            }
+        } as T
+    }
+}
---
+++ app/src/main/kotlin/com/google/ai/soga/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.google.ai.soga
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.ai.soga.feature.chat.ChatRoute
+import com.google.ai.soga.ui.theme.GenerativeAISample
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            GenerativeAISample {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "menu") {
+                        composable("menu") {
+                            MenuScreen(onItemClicked = { routeId ->
+                                navController.navigate(routeId)
+                            })
+                        }
+                        composable("chat") {
+                            ChatRoute()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
